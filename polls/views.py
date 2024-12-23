f65c073fef3007c2a9a66d448674d0503d46ce3b
mailrun <mailrun@sohu.com>
2024-12-23 17:57:01 +0800

    37

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.template import loader
 from django.http import HttpResponse
-
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # 载入 polls/index.html 模板文件，并且向它传递一个上下文(context)。这个上下文是一个字典，它将模板内的变量映射为 Python 对象。
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
