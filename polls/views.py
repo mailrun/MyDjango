aedaba5fcd0cd6b63f73977665583e97546d157a
mailrun <mailrun@sohu.com>
2024-12-23 18:41:33 +0800

    38

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,20 @@
-from django.template import loader
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
 from .models import Question
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     # 载入 polls/index.html 模板文件，并且向它传递一个上下文(context)。这个上下文是一个字典，它将模板内的变量映射为 Python 对象。
+#     template = loader.get_template("polls/index.html")
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    # 载入 polls/index.html 模板文件，并且向它传递一个上下文(context)。这个上下文是一个字典，它将模板内的变量映射为 Python 对象。
-    template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
